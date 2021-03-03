171e493a7fcd811d1de045471304d79ecc5684de
ricesar21 <ricesar21@gmail.com>
2021-03-02 22:34:45 -0300

    rotas

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,14 @@
-const express = require('express');
-const app = express();
-const port = 3000
-const bodyParser = require('body-parser')
-const { Filme } = require('./models');
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const indexRouter = require('./routes/index');
+const filmesRouter = require('./routes/filmes');
 
 app.use(bodyParser.json());
 
-app.get('/', async (req, res) => {
-    res.send('running :P');
-});
+app.use('/', indexRouter);
+app.use('/filmes', filmesRouter);
 
-app.get('/filmes', async (req, res) => {
-    const filmes = await Filme.findAll();
-    res.status(200).json(filmes);
-});
-app.get('/filmes/:id', async (req, res) => {
-    const filmes = await Filme.findAll({
-        where: {
-
-            id: req.params.id
-        }
-
-    });
-    res.status(200).json(filmes)
-});
-
-app.post('/filmes', async (req, res) => {
-    const filmes = await Filme.create(req.body)
-
-    res.status(201).json(filmes)
-
-});
-
-app.put('/filmes/:id', async (req, res) => {
-    const filmes = await Filme.update(req.body,{
-        where: {
-            id: req.params.id
-        }
-    });
-    res.status(200).json(filme);
-});
-
-
-app.listen(process.env.PORT || port);
+app.listen(process.env.PORT || 3000);
---
+++ backend/routes/filmes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const { Filme } = require('../models');
+
+router.get('/', async (req, res) => {
+    const filmes = await Filme.findAll();
+    res.status(200).json(filmes);
+});
+
+router.get('/:id', async (req, res) => {
+    const filmes = await Filme.findAll({
+        where: {
+
+            id: req.params.id
+        }
+    });
+    res.status(200).json(filmes)
+});
+
+router.post('/', async (req, res) => {
+    const filmes = await Filme.create(req.body)
+
+    res.status(201).json(filmes)
+
+});
+
+router.put('/:id', async (req, res) => {
+    const filmes = await Filme.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    });
+    res.status(200).json(filme);
+});
+
+module.exports = router
