b46b2625ad35e9e7b958f52b3fa6475d0a4c55ca
Jukita <zjukiita@gmail.com>
2021-03-03 21:05:22 -0300

    Login E Autenticação

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,15 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const indexRouter = require('./routes/index');
+const userRouter = require ('./routes/user');
+const authRouter = require ('./routes/auth');
 const filmesRouter = require('./routes/filmes');
 
 app.use(bodyParser.json());
 
 app.use('/', indexRouter);
+app.use('/users', userRouter);
+app.use('/login', authRouter);
 app.use('/filmes', filmesRouter);
 
 app.listen(process.env.PORT || 3000);
