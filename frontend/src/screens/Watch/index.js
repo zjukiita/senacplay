5036b5100926b58782195a55ba13a85a0fbc84c4
V1NNI7 <vinicezar800@hotmail.com>
2021-03-10 18:09:41 -0300

    Criação da Watch

--- frontend/src/screens/Watch/index.js
+++ frontend/src/screens/Watch/index.js
@@ -1,3 +1,44 @@
-import React from 'react';
-import classes from './watch.module.css'
-import Logo from '../../movies/Titanic'
+import React, { useEffect, useState } from 'react';
+import Movie from '../../components/WatchMovie';
+import api from '../../services/api'
+
+const Watch = () => {
+    const [movies, setMovies] = useState([]);
+    const [search, setSearch] = useState('');
+    const loadingMovies = async () => {
+        const response = await api.get(`/filmes`); //Chave de busca da API
+        setMovies([]);
+        if (response.data)
+            setMovies(response.data);
+    }
+    useEffect(() => {
+        loadingMovies();
+    }, []);
+    return (
+        <>
+            {/* <div>
+                <img />
+                <div>
+                    <input
+                        id="search"
+                        name="search"
+                        type="text"
+                        placeholder="Pesquisa"
+                        value={search}
+                        onChange={(e) => { setSearch(e.target.value) }}
+                    />
+                </div>
+            </div> */}
+            {movies.map(m => {
+                return (
+                    <div key={m.id}>
+                        <Movie movie={m}
+                        />
+                    </div>
+                );
+            })}
+        </>
+    );
+}
+
+export default Watch;
