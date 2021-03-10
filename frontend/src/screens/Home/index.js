a812ea40b8a4c455e6f869ee760a23085461e919
V1NNI7 <vinicezar800@hotmail.com>
2021-03-10 01:20:29 -0300

    Bugs Corrigidos

--- frontend/src/screens/Home/index.js
+++ frontend/src/screens/Home/index.js
@@ -8,61 +8,63 @@ const Home = () => {
         <>
             <body className={classes.body}>
 
-                <div className={classes.principalPage}>
-                    <nav className={classes.homeNavBar} style={{ paddingInline: 25 }}  >
+                    <div className={classes.principalPage}>
+                        
+                        <nav className={classes.homeNavBar} style={{ paddingInline: 25 }}  >
                             <img className={classes.imgLogo} src={Logo} width="50" height="50" />
                             <Link to="/login"><button type="button" class className={classes.loginButton}>Entrar</button></Link>
-                    </nav>
+                        </nav>
 
-                    <div className={classes.initialSection}>
-                        <h4 class="mt-5">Tá afim de assistir um filme de ação? ou quem sabe um filme clássico? até mesmo uma comédia?</h4>
-                        <h5>Crie sua conta no Senac Play e assista filmes de graça!</h5>
-                        <Link to="/register"><button type="button" className={classes.registerButton}>Criar conta</button></Link>
+                        <div className={classes.initialSection}>
+                            <h4 class="mt-5">Tá afim de assistir um filme de ação? ou quem sabe um filme clássico? até mesmo uma comédia?</h4>
+                            <h5>Crie sua conta no Senac Play e assista filmes de graça!</h5>
+                            <Link to="/register"><button type="button" className={classes.registerButton}>Criar conta</button></Link>
+                        </div>
+
                     </div>
-                </div>
 
-                <hr class="container-fluid divisor p-2 bg-secundary mt-0 mb-0" />
+                    <hr class="container-fluid divisor p-2 bg-secundary mt-0 mb-0" />
 
-                <div class="container-fluid mt-0" className={classes.secondSection}>
-                    <h4>Aproveite filmes em seu navegador!</h4>
-                    <h5>Filmes de todos os tipos em alta qualidade e atualizados!</h5>
-                </div>
+                    <div class="container-fluid mt-0" className={classes.secondSection}>
+                        <h4>Aproveite filmes em seu navegador!</h4>
+                        <h5>Filmes de todos os tipos em alta qualidade e atualizados!</h5>
+                    </div>
 
-                <hr class="container-fluid divisor p-2 bg-secundary mt-0 mb-0" />
+                    <hr class="container-fluid divisor p-2 bg-secundary mt-0 mb-0" />
 
-                <div class="container-fluid" className={classes.thirdSection}>
-                    <h4>Assista em qualquer lugar e a qualquer hora</h4>
-                    <h5>Temos nosso próprio aplicativo para celular para você assistir em qualquer lugar</h5>
-                </div>
+                    <div class="container-fluid" className={classes.thirdSection}>
+                        <h4>Assista em qualquer lugar e a qualquer hora</h4>
+                        <h5>Temos nosso próprio aplicativo para celular para você assistir em qualquer lugar</h5>
+                    </div>
 
-                <hr class="container-fluid divisor p-2 bg-secundary mt-0 mb-0" />
+                    <hr class="container-fluid divisor p-2 bg-secundary mt-0 mb-0" />
 
-                <div class="container-fluid" className={classes.fourSection}>
-                    <h4>Aplicativo Desktop</h4>
-                    <h5>Baixe nosso aplicativo Desktop para um maior conforto!</h5>
-                </div>
+                    <div class="container-fluid" className={classes.fourSection}>
+                        <h4>Aplicativo Desktop</h4>
+                        <h5>Baixe nosso aplicativo Desktop para um maior conforto!</h5>
+                    </div>
 
-                <hr class="container-fluid divisor p-2 bg-secundary mt-0 mb-0" />
+                    <hr class="container-fluid divisor p-2 bg-secundary mt-0 mb-0" />
 
-                <div class="container-fluid row" className={classes.informationSection}>
-                    <div class="row text-center text-white">
-                        <div class="col-sm-4">
-                            <p>Tire suas dúvidas</p>
-                            <p>Entre em contato</p>
-                            <p>Redes Sociais</p>
-                        </div>
-                        <div class="col-sm-4">
-                            <p>Termos de uso</p>
-                            <p>Perguntas frequentes</p>
-                            <p>Formas de assistir</p>
-                        </div>
-                        <div class="col-sm-4">
-                            <p>Conta</p>
-                            <p>Privacidade</p>
-                            <p>Aplicativos</p>
+                    <div class="container-fluid row" className={classes.informationSection}>
+                        <div class="row text-center text-white">
+                            <div class="col-sm-4">
+                                <p>Tire suas dúvidas</p>
+                                <p>Entre em contato</p>
+                                <p>Redes Sociais</p>
+                            </div>
+                            <div class="col-sm-4">
+                                <p>Termos de uso</p>
+                                <p>Perguntas frequentes</p>
+                                <p>Formas de assistir</p>
+                            </div>
+                            <div class="col-sm-4">
+                                <p>Conta</p>
+                                <p>Privacidade</p>
+                                <p>Aplicativos</p>
+                            </div>
                         </div>
                     </div>
-                </div>
             </body>
         </>
     );
