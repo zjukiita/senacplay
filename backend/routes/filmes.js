0cdacf246b771b3d30c4d8e8f55424e66656bbfe
Jukita <zjukiita@gmail.com>
2021-03-10 13:55:54 -0300

    Com Categoria(n funfa)

--- backend/routes/filmes.js
+++ backend/routes/filmes.js
@@ -30,7 +30,17 @@ router.get('/nome/:nome', async (req, res) => {
     res.status(200).json(filmes);
 });
 
+router.get('/categoria/:categoria', async (req,res) => {
+    const filmes = await Filme.findAll({
+        where: {
+            nome: {
+                [Op.like]: `${req.params.categoria}%`
+            }
+        }
 
+    });
+    res.status(200).json(filmes);
+});
 
 router.post('/', async (req, res) => {
     const filmes = await Filme.create(req.body)
