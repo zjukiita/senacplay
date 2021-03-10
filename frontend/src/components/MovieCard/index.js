4055e5c7b292126bd673c3f0675621115d8d71e3
V1NNI7 <vinicezar800@hotmail.com>
2021-03-10 02:09:35 -0300

    Listagem de filmes por nome funcionando

--- frontend/src/components/MovieCard/index.js
+++ frontend/src/components/MovieCard/index.js
@@ -4,14 +4,15 @@ import classes from './moviecard.module.css';
 const MovieCard = (props) => {
     return (
         <div className={classes.Card}>
-            <img src={props.movie.imagem} width="360" height="530" /* alt={props.movie.nome} */ />
+            <a src={props.movie.link}><img src={props.movie.imagem} width="360" height="530" /* alt={props.movie.nome} */ /></a>
             <div class="container-fluid" className={classes.words}>
                 <h2>{props.movie.nome}</h2>
                 <p><label>Ano: </label> {props.movie.anoDeLancamento}</p>
-                <p><label>Categoria: </label>{props.movie.categoria}</p>
-                <p><label>Sinopse: </label>{props.movie.sinopse}</p>
-                <p><label>Duração: </label>{props.movie.duracao}</p>
-                <p><label>Avaliação: </label>{props.movie.avaliacao}</p>
+                <p><label>Categoria: </label> {props.movie.categoria}</p>
+                <p><label>Sinopse: </label> {props.movie.sinopse}</p>
+                <p><label>Duração: </label> {props.movie.duracao}</p>
+                <p><label>Avaliação: </label> {props.movie.avaliacao}</p>
+                <p><label>Links: </label> {props.movie.link}</p>
             </div>
         </div>
     )
