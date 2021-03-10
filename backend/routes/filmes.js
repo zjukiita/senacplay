cc405ed341e0fb050dfa37e47e904816bde30287
Jukita <zjukiita@gmail.com>
2021-03-10 12:14:48 -0300

    Rota para pesquisar filmes por category

--- backend/routes/filmes.js
+++ backend/routes/filmes.js
@@ -30,6 +30,19 @@ router.get('/nome/:nome', async (req, res) => {
     res.status(200).json(filmes);
 });
 
+
+router.get('/categoria/:categoria', async (req,res) => {
+    const filmes = await Filme.findAll({
+        where: {
+            nome: {
+                [Op.like]: `${req.params.categoria}%`
+            }
+        }
+
+    });
+    res.status(200).json(filmes);
+});
+
 router.post('/', async (req, res) => {
     const filmes = await Filme.create(req.body)
     res.status(201).json(filmes)
